6669e6b8a2ff1f99ed0e9170f646ed390f7d164c
Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
2019-12-26 13:29:04 +0900

    add logerr.sh
    logerr indicates warning/error from log file
    
    Signed-off-by: Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>

---
+++ script/logerr.sh
@@ -0,0 +1,8 @@
+#! /bin/sh
+#===============================
+#
+# logerr
+#
+# 2019/12/17 Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
+#===============================
+egrep -v "(^  GEN|^  UPD|^  HDRINST|^  CC|^  CALL|^  AR|^  CHK|^  LD|^  GZIP|^  MODPOST|^  MODINFO|^  KSYM|^  SYSMAP|^  Building|^  DESCEND|^  HDRTEST|^  X32|^  VDSO|^  OBJCOPY|^  VDSO2C|^  EXPORTS|^  UNROLL|^  BIN2C|^  SORTEX|^  VOFFSET|^  RELOCS|^  HOSTCC|^  HOSTLD|^  MKPIGGY|^  AS|^  ZOFFSET|^  BUILD|^Setup|^System|^CRC|^scripts|^Kernel|^make)" $@
