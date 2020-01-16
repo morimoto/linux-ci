157d2c79e743db31c2e242afcfaa5f07530dfc8f
morimoto <kuninori.morimoto.gx@renesas.com>
2020-01-16 10:15:49 +0900

    logerr: add more non-error prefix
    Signed-off-by: morimoto <kuninori.morimoto.gx@renesas.com>

--- script/logerr.sh
+++ script/logerr.sh
@@ -5,4 +5,4 @@
 #
 # 2019/12/17 Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
 #===============================
-egrep -v "(^  GEN|^  UPD|^  HDRINST|^  CC|^  CALL|^  AR|^  CHK|^  LD|^  GZIP|^  MODPOST|^  MODINFO|^  KSYM|^  SYSMAP|^  Building|^  DESCEND|^  HDRTEST|^  X32|^  VDSO|^  OBJCOPY|^  VDSO2C|^  EXPORTS|^  UNROLL|^  BIN2C|^  SORTEX|^  VOFFSET|^  RELOCS|^  HOSTCC|^  HOSTLD|^  MKPIGGY|^  AS|^  ZOFFSET|^  BUILD|^Setup|^System|^CRC|^scripts|^Kernel|^make)" $@
+egrep -v "(^  GEN|^  UPD|^  HDRINST|^  CC|^  CALL|^  AR|^  CHK|^  LD|^  GZIP|^  MODPOST|^  MODINFO|^  KSYM|^  SYSMAP|^  Building|^  DESCEND|^  HDRTEST|^  X32|^  VDSO|^  OBJCOPY|^  VDSO2C|^  EXPORTS|^  UNROLL|^  BIN2C|^  SORTEX|^  VOFFSET|^  RELOCS|^  HOSTCC|^  HOSTLD|^  MKPIGGY|^  AS|^  ZOFFSET|^  BUILD|^  TEST|^  Kernel:|^  DTC|^  ITS|^  ITB|^  WRAP|^Setup|^System|^CRC|^scripts|^Kernel|^make)" $@
