1e6b1d0d4d58ddd87da3bfa2378cb57f97b602d9
Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
2021-12-01 15:16:12 +0900

    Add tool-install-ubuntu.sh
    It needs some tools. This patch adds easy install script for it.
    
    Signed-off-by: Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>

---
+++ script/tool-install-ubuntu.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+#===============================
+#
+# tool-install-ubuntu
+#
+# 2021/12/01 morimoto <morimoto@venus>
+#===============================
+sudo apt -y install \
+     ccache s-nail sendmail g++ make bison flex \
